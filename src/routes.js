b04af39cf21cb30abb3be5a7c49a96c17241c4b7
Miftahul Anwar <miftahulanwar129@gmail.comr>
2023-07-21 18:15:15 +0700

    inital commit

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNotesByIdHandler,
+  editNoteByHandler,
+  deleteNoteByHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNotesByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByHandler,
+  },
+];
+module.exports = routes;
